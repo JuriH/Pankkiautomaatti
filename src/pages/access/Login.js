02947ef5073201f09bb906e236cbd53bbf98699d
root <juri.haataja@hotmail.com>
2021-03-05 06:22:57 +0000

    First commit

---
+++ src/pages/access/Login.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import "./login.css";
+import "../../fonts.css";
+
+export default function Login() {
+  return (
+    <div className="container">
+      <div className="formContainer">
+        <div className="logo">Paste</div>
+        <div className="form">
+        </div>
+      </div>
+    </div>
+  );
+}
